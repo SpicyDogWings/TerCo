cbf55184dc311e78c3202b247710797f05b14184
HarmonySpace <jchavaspace@gmail.com>
2025-02-26 21:15:10 -0600

    init view

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="w-full flex flex-col justify-center items-center bg-slate-100">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-full my-20 flex flex-col justify-start items-start">
+    <div class="w-full flex justify-between items-center">
+      <h1 class="text-5xl font-mono font-bold">TerCo</h1>
+      <div class="flex justify-center items-center gap-"></div>
+    </div>
+  </div>
+</template>
