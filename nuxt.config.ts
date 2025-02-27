6c22bd5060828051fc78c357cc38f711766c98fa
HarmonySpace <jchavaspace@gmail.com>
2025-02-26 20:34:21 -0600

    feat(dep): agregado el stack general

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@vueuse/nuxt",
+    "@nuxtjs/color-mode",
+    "@pinia/nuxt",
+  ],
+  ssr: false,
+  vite: { plugins: [tailwindcss()] },
+  css: ["~/assets/css/main.css"],
+});
